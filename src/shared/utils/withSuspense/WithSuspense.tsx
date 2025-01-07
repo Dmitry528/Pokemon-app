770170c3ef1f5c381f86a2d069ec67fa81227a5c
Dmitry528 <dmytrochekurkov@gmail.com>
2025-01-07 23:31:14 +0200

    fix import

---
+++ src/shared/utils/withSuspense/WithSuspense.tsx
@@ -0,0 +1,18 @@
+import { ComponentType, Suspense } from 'react';
+
+const withSuspense = <P extends JSX.IntrinsicAttributes>(
+  Component: ComponentType<P>,
+  fallback: JSX.Element,
+) => {
+  const InnerSuspenseComponent = (props: P) => {
+    return (
+      <Suspense fallback={fallback}>
+        <Component {...props} />
+      </Suspense>
+    );
+  };
+
+  return InnerSuspenseComponent;
+};
+
+export default withSuspense;
